b26c780a6e6c99c92037978afb6c09ce1283bae5
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-08-14 09:35:23 +0300

    Add seed and routes

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,11 @@
+class MessagesController < ApplicationController
+    def index
+    end
+
+    def random
+        @message = Message.find(Message.pluck(:id).sample)
+
+        render json: @message
+
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Message.create(text: "Hi there!")
+Message.create(text: "Final Week")
+Message.create(text: "lets gooooo!")
+Message.create(text: "rails and react")
+Message.create(text: "here we go!")
